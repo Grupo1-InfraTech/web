b8d62ee4ceab0bf78e392d932a6176a9ea3e7313
Renata Antequiera <rantequierar@correo.uss.cl>
2025-06-07 17:45:46 -0400

    Modificacion del main.ts para los componentes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { appConfig } from './app/app.config';
+import { routes } from './app/app.routes';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { LOCALE_ID } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+registerLocaleData(localeEs);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    ...appConfig,
+    { provide: LOCALE_ID, useValue: 'es' }
+  ]
+});
